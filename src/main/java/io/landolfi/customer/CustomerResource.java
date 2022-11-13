e431d42af178cf0663ee5c56885fb75912157482
Nico769 <ncl.lnd769@gmail.com>
2022-11-13 12:04:09 +0100

    add test for retrieving all `Customer` resources assuming that only one has been previously POSTed

--- src/main/java/io/landolfi/customer/CustomerResource.java
+++ src/main/java/io/landolfi/customer/CustomerResource.java
@@ -1,5 +1,6 @@
 package io.landolfi.customer;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -19,5 +20,10 @@ public class CustomerResource {
                 received.name(), received.surname(), received.fiscalCode(), received.address());
         customers.put(toReturn.uuid(), toReturn);
         return Response.created(URI.create("/customers/c8a255af-208d-4a98-bbff-8244a7a28609")).entity(toReturn).build();
+    }
+
+    @GET
+    public CustomersDto retrieveAllCustomers() {
+        return new CustomersDto(customers.values().stream().toList());
     }
 }
